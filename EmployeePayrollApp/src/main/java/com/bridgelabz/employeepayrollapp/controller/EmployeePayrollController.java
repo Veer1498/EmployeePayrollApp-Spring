eabbd77690fbab0e872e396e02567af7f7873c8d
Veer1498 <singavarapuveerendra@gmail.com>
2022-10-14 13:18:49 +0530

    [Veer] Added : To Create Rest Controller and set Required Properties to take HTTP Requests

---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * Puprose : To Create Employee Payroll App and Perform Operations.
+ * Author : Veer.Singa
+ */
+@RestController
+@RequestMapping("/home")
+public class EmployeePayrollController {
+
+}
