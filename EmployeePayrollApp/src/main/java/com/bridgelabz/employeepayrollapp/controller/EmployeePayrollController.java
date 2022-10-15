452ad7e76961e815402c4beddb68847af06b7520
Veer1498 <singavarapuveerendra@gmail.com>
2022-10-15 23:31:14 +0530

    [Veer] Added : Tried with personal logic for UC3

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/controller/EmployeePayrollController.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.employeepayrollapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeeData;
+import org.springframework.web.bind.annotation.*;
+
+
 /**
  * Puprose : To Create Employee Payroll App and Perform Operations.
  * Author : Veer.Singa
@@ -10,4 +13,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/home")
 public class EmployeePayrollController {
 
+    /**
+     *
+     * @param empDTO
+     * @return - empData
+     */
+    @PostMapping("/insertData")
+    public EmployeeData insertData(@RequestBody EmployeePayrollDTO empDTO){
+        EmployeeData empData = new EmployeeData(1,empDTO.empName,empDTO.empSalary);
+        return empData;
+    }
+
+    /**
+     *
+     * @param id
+     * @return Employee Object
+     */
+    @GetMapping("/GetByID/{id}")
+    public EmployeeData getEmployeeByID(@PathVariable long id){
+        long idn = 2;
+        EmployeeData empData = new EmployeeData((int) idn,"Veer",35000);
+        if (idn == id){
+            return empData;
+        }
+        return null;
+    }
 }
---
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.employeepayrollapp.dto;
+
+public class EmployeePayrollDTO {
+    public String empName;
+    public long empSalary;
+
+
+}
