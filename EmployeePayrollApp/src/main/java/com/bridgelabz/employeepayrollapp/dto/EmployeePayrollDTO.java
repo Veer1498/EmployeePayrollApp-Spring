680acb8e630e61320b4692c8e73a8222c35b8ecf
Veer1498 <singavarapuveerendra@gmail.com>
2022-10-19 01:09:16 +0530

    [Veer] Added : Ability to ensure Validations for all properties

--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -1,10 +1,9 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.util.Date;
 import java.util.List;
 
@@ -13,17 +12,25 @@ public class EmployeePayrollDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
     @NotEmpty(message = "Employee Name should not be Null")
     private String name;
+
     @Min(value = 500,message = "Minimum Wage should be 500 or More")
     private long salary;
 
+    @Pattern(regexp = "male|female|others",message = "Gender Needs to be Male,Female or Others")
     private String gender;
 
+    @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "startDate Should not be Empty")
+    @PastOrPresent(message = "startDate Should not be past or present Date")
     private Date startDate;
 
+    @NotBlank(message = "Note cannot be empty")
     private String note;
 
+    @NotBlank(message = "Profile Pic should not be Empty")
     private String profilePic;
 
+    @NotNull(message = "department should not be Empty")
     private List<String> department;
 
 
--- EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
+++ EmployeePayrollApp/src/main/java/com/bridgelabz/employeepayrollapp/exception/EmployeePayrollExceptionHandler.java
@@ -3,6 +3,7 @@ package com.bridgelabz.employeepayrollapp.exception;
 import com.bridgelabz.employeepayrollapp.dto.ResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
 
 
 @ControllerAdvice
@@ -34,6 +36,13 @@ public class EmployeePayrollExceptionHandler {
     public ResponseEntity<ResponseDTO> handleEmployeePayrollException(EmployeePayrollException exception){
         ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", exception.getMessage());
         return new ResponseEntity<>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+    private static  final String message = "Exception while Prosessing REST Request";
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ResponseDTO> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception){
+//        log.error("Invalid Date Format",exception);
+        ResponseDTO responseDTO = new ResponseDTO(message,"Should have date in the Format of dd MM yyyy");
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
     }
 
 }
